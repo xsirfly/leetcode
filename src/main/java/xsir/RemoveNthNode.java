1c36827af5e21bb54014ef188035343a29120355
xsir <xmanzc@163.com>
2018-10-09 15:41:30 +0800

    up

---
+++ src/main/java/xsir/RemoveNthNode.java
@@ -0,0 +1,45 @@
+package xsir;
+
+/**
+ * @author zhangcong
+ * @date 2018/10/9
+ */
+public class RemoveNthNode {
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        public ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = null, cur, end;
+        cur = end = head;
+        while (n > 1 && end != null) {
+            end = end.next;
+            n--;
+        }
+        if (end == null) {
+            return head.next;
+        }
+        while (end.next != null) {
+            pre = cur;
+            cur = cur.next;
+            end = end.next;
+        }
+        // just
+        if (pre == null) {
+            return cur.next;
+        }
+        pre.next = cur.next;
+        return head;
+    }
+
+}
