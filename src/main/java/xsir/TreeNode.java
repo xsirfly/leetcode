3032cefee0904166a829e5565e43d5242193e41b
xsir <xmanzc@163.com>
2019-04-29 15:10:47 +0800

    up

---
+++ src/main/java/xsir/TreeNode.java
@@ -0,0 +1,14 @@
+package xsir;
+
+/**
+ * @author zhangcong
+ * @date 2019/4/29
+ */
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+
+}
